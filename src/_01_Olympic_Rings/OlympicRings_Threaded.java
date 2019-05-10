d5ac0d1fec2632789531d9e5f8f26d55f8f0ea33
League Student <league@iMac-96.attlocal.net>
2019-05-09 18:29:04 -0700

    ThreadedPool

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,105 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
 
+		Robot one = new Robot(300, 300);
+		Robot two = new Robot(350, 350);
+		Robot three = new Robot(400, 300);
+		Robot four = new Robot(450, 350);
+		Robot five = new Robot(500, 300);
+
+		one.setSpeed(10);
+		two.setSpeed(10);
+		three.setSpeed(10);
+		four.setSpeed(10);
+		five.setSpeed(10);
+
+		one.setPenWidth(5);
+		two.setPenWidth(5);
+		three.setPenWidth(5);
+		four.setPenWidth(5);
+		five.setPenWidth(5);
+
+		one.penDown();
+		two.penDown();
+		three.penDown();
+		four.penDown();
+		five.penDown();
+
+		one.setPenColor(Color.BLUE);
+		two.setPenColor(Color.YELLOW);
+		three.setPenColor(Color.BLACK);
+		four.setPenColor(Color.GREEN);
+		five.setPenColor(Color.RED);
+
+		Thread r1 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				one.setAngle(i);
+				one.move(1);
+			}
+		});
+		Thread r2 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				two.setAngle(i);
+				two.move(1);
+			}
+		});
+		Thread r3 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				three.setAngle(i);
+				three.move(1);
+			}
+		});
+		Thread r4 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				four.setAngle(i);
+				four.move(1);
+			}
+		});
+		Thread r5 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				five.setAngle(i);
+				five.move(1);
+			}
+		});
+		
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+		
+		try {
+			r1.join();
+			r2.join();
+			r3.join();
+			r4.join();
+			r5.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		one.penUp();
+		two.penUp();
+		three.penUp();
+		four.penUp();
+		five.penUp();
+		one.setAngle(0);
+		two.setAngle(0);
+		three.setAngle(0);
+		four.setAngle(0);
+		five.setAngle(0);
+		one.move(-300);
+		two.move(-300);
+		three.move(-300);
+		four.move(-300);
+		five.move(-300);
+	}
 }
-
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,178 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
+	// Re-do the robot race recipe from level 3 module 0.
 	// This time, use threads to make all of the robots go at the same time.
+	// 1. make a main method
+	public static void main(String[] args) {
+
+		// 2. create an array of 5 robots.
+		Robot[] robots = new Robot[5];
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot();
+		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
+		for (int i = 0; i < robots.length; i++) {
+			robots[i].setSpeed(20);
+			robots[i].move(-200);
+		}
+		robots[1].turn(90);
+		robots[1].move(200);
+		robots[1].turn(-90);
+		robots[2].turn(90);
+		robots[2].move(100);
+		robots[2].turn(-90);
+		robots[3].turn(-90);
+		robots[3].move(200);
+		robots[3].turn(90);
+		robots[4].turn(-90);
+		robots[4].move(100);
+		robots[4].turn(90);
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+		/*
+		 * Random random = new Random(50); for (int i = 0; i < robots.length; i++) { int
+		 * number = random.nextInt(50); robots[i].move(number); }
+		 */
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
+		Random random = new Random(50);
+		boolean notreached = true;
+
+		Thread r1 = new Thread(() -> {
+			while (notreached) {
+				int number = random.nextInt(50);
+				robots[0].move(number);
+				if (robots[0].getY() < 0) {
+					System.out.println("Robot " + 1 + " is the winner!");
+					break;
+				}
+			}
+		});
+		Thread r2 = new Thread(() -> {
+			while (notreached) {
+				int number = random.nextInt(50);
+				robots[1].move(number);
+				if (robots[1].getY() < 0) {
+					System.out.println("Robot " + 2 + " is the winner!");
+					break;
+				}
+			}
+		});
+		Thread r3 = new Thread(() -> {
+			while (notreached) {
+				int number = random.nextInt(50);
+				robots[2].move(number);
+				if (robots[2].getY() < 0) {
+					System.out.println("Robot " + 3 + " is the winner!");
+					break;
+				}
+			}
+		});
+		Thread r4 = new Thread(() -> {
+			while (notreached) {
+				int number = random.nextInt(50);
+				robots[3].move(number);
+				if (robots[3].getY() < 0) {
+					System.out.println("Robot " + 4 + " is the winner!");
+					break;
+				}
+			}
+		});
+		Thread r5 = new Thread(() -> {
+			while (notreached) {
+				int number = random.nextInt(50);
+				robots[4].move(number);
+				if (robots[4].getY() < 0) {
+					System.out.println("Robot " + 5 + " is the winner!");
+					break;
+				}
+			}
+		});
+
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+		// 7. declare that robot the winner and throw it a party!
+
+		// 8. try different races with different amounts of robots.
+
+		// 9. make the robots race around a circular track.
+
+		try {
+			r1.join();
+			r2.join();
+			r3.join();
+			r4.join();
+			r5.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < robots.length; i++) {
+			robots[i].moveTo(450, 500);
+			robots[i].turn(90);
+		}
+
+		Thread t1 = new Thread(() -> {
+			int moves = random.nextInt(100);
+			for (int g = 0; g < moves; g++) {
+				move(robots[0]);
+			}
+		});
+		Thread t2 = new Thread(() -> {
+
+			int moves = random.nextInt(100);
+			for (int g = 0; g < moves; g++) {
+				move(robots[1]);
+			}
+
+		});
+		Thread t3 = new Thread(() -> {
+
+			int moves = random.nextInt(100);
+			for (int g = 0; g < moves; g++) {
+				move(robots[2]);
+			}
+
+		});
+		Thread t4 = new Thread(() -> {
+			for (int i = 0; i < robots.length; i++) {
+				int moves = random.nextInt(100);
+				for (int g = 0; g < moves; g++) {
+					move(robots[3]);
+				}
+			}
+		});
+		Thread t5 = new Thread(() -> {
+
+			int moves = random.nextInt(100);
+			for (int g = 0; g < moves; g++) {
+				move(robots[4]);
+			}
+
+		});
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+		
+	}
+
+		
+	static void move(Robot robot) {
+		robot.move(15);
+		robot.turn(-5);
+	}
+
 }
